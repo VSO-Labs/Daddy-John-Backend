3ad3995bfe078ece81047a8eba0b3b8494ec9f87
sankethanse <120187322+sohamhanse@users.noreply.github.com>
2025-08-09 19:41:39 +0530

    Initial commit: Spring Boot project setup

---
+++ DaddyJohn/DaddyJohn/src/main/java/com/vso/DaddyJohn/DaddyJohnApplication.java
@@ -0,0 +1,13 @@
+package com.vso.DaddyJohn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaddyJohnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaddyJohnApplication.class, args);
+	}
+
+}
---
+++ DaddyJohn/DaddyJohn/src/test/java/com/vso/DaddyJohn/DaddyJohnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vso.DaddyJohn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaddyJohnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
