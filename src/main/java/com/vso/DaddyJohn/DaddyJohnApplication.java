111fd1a841764f697f6ae8d5760f28922802cedf
Soham Hanse <120187322+sohamhanse@users.noreply.github.com>
2025-08-31 11:58:51 +0530

    Update DaddyJohnApplication.java

--- src/main/java/com/vso/DaddyJohn/DaddyJohnApplication.java
+++ src/main/java/com/vso/DaddyJohn/DaddyJohnApplication.java
@@ -3,11 +3,11 @@ package com.vso.DaddyJohn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DaddyJohnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DaddyJohnApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DaddyJohnApplication.class, args);
+    }
 }
+
