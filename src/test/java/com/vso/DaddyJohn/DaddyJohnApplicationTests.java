9197cc95d677a3ad8d747332a03ba08637e9b2fb
soham <sohamh_c22087@students.isquareit.edu.in>
2025-09-02 23:37:03 +0530

    Done some changes to resolve the messgeservice

--- src/test/java/com/vso/DaddyJohn/DaddyJohnApplicationTests.java
+++ src/test/java/com/vso/DaddyJohn/DaddyJohnApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DaddyJohnApplicationTests {
+class 	DaddyJohnApplicationTests {
 
 	@Test
 	void contextLoads() {
