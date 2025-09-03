e5fc2b985d15fcadbc4df4370f2d58e658d92a7e
omkar <sonawaneomkar2306@gmail.com>
2025-09-03 13:27:41 +0530

    Fixed Everything

--- src/main/java/com/vso/DaddyJohn/Dto/CreateMessageRequest.java
+++ src/main/java/com/vso/DaddyJohn/Dto/CreateMessageRequest.java
@@ -4,5 +4,13 @@ import lombok.Data;
 
 @Data
 public class CreateMessageRequest {
-    private String content;
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
